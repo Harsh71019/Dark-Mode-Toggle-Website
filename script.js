fb5d582eeea450cb6adf360006ca43ac1821c586
harsh <harsh710pat@gmail.com>
2020-07-25 17:52:49 +0530

    Hey enablng Dark Mode Functonalty

---
+++ script.js
@@ -0,0 +1,47 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById("nav");
+const image1 = document.getElementById("image1");
+const image2 = document.getElementById("image2");
+const image3 = document.getElementById("image3");
+const toggleIcon = document.getElementById("toggle-icon");
+const textBox = document.getElementById("text-box");
+
+//Dark or Light Images
+function imageMode(color) {
+  image1.src = `img/undraw_proud_coder_${color}.svg`;
+  image2.src = `img/undraw_feeling_proud_${color}.svg`;
+  image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
+function darkMode() {
+  nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
+  textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
+  console.log(toggleIcon.children);
+  toggleIcon.children[0].textContent = "Dark Mode";
+  toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
+  // toggleIcon.children[1].classList.add('fa-moon')
+  imageMode("dark");
+}
+
+function lightMode() {
+  nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
+  textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
+  console.log(toggleIcon.children);
+  toggleIcon.children[0].textContent = "Light Mode";
+  toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+  imageMode("light");
+}
+
+function switchTheme() {
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    darkMode();
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    lightMode();
+  }
+}
+
+//Event Listener
+
+toggleSwitch.addEventListener("change", switchTheme);
